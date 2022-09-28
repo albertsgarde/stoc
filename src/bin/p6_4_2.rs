8f79828a14da2ef011d26df91391a0a304928c4f
Albert Garde <albertsgarde@gmail.com>
2022-09-28 13:02:46 +0200

    :tada:

---
+++ src/bin/p6_4_2.rs
@@ -0,0 +1,127 @@
+#![allow(unused_imports)]
+use std::{
+    fmt::Display,
+    iter::Sum,
+    ops::{Add, Div},
+};
+
+use stoc::{test_theory, Vector};
+
+use float_cmp::approx_eq;
+use ndarray::Array1;
+use num::{traits::Pow, Float};
+use rand::{Rng, SeedableRng};
+use rand_distr::{Bernoulli, Distribution, Exp, Poisson, Uniform};
+use rand_pcg::Pcg64Mcg;
+use rayon::prelude::{IntoParallelIterator, ParallelIterator};
+
+const SEED: u128 = 5;
+const MAX_THREADS: u32 = 8;
+
+struct Parameters {
+    lambda: f64,
+    mu: f64,
+    n: usize,
+    sample_start: f64,
+    sample_end: f64,
+}
+
+fn advance_state(
+    parameters: &Parameters,
+    cur_state: &mut usize,
+    cur_time: &mut f64,
+    rng: &mut impl Rng,
+) -> f64 {
+    let &Parameters {
+        lambda,
+        mu,
+        n,
+        sample_start,
+        sample_end,
+    } = parameters;
+    if *cur_state == 0 {
+        let birth_time = Exp::new(lambda).unwrap().sample(rng);
+        *cur_time += birth_time;
+        *cur_state += 1;
+        birth_time
+    } else {
+        let sojourn_time = Exp::new(lambda + mu).unwrap().sample(rng);
+        let death = Bernoulli::new(mu / (lambda + mu)).unwrap().sample(rng);
+        *cur_time += sojourn_time;
+        if death {
+            *cur_state -= 1;
+        } else {
+            *cur_state += 1;
+        }
+        sojourn_time
+    }
+}
+
+fn experiment(parameters: &Parameters, rng: &mut impl Rng) -> Vector {
+    let &Parameters {
+        lambda,
+        mu,
+        n,
+        sample_start,
+        sample_end,
+    } = parameters;
+    let mut cur_time = 0.;
+    let mut cur_state = 0usize;
+    let mut prev_state = 0usize;
+    while cur_time <= sample_start {
+        prev_state = cur_state;
+        advance_state(parameters, &mut cur_state, &mut cur_time, rng);
+    }
+    let mut time_in_states = Vector::from_elem(n, 0.);
+    if prev_state < n {
+        time_in_states[prev_state] += cur_time - sample_start;
+    }
+
+    while cur_time <= sample_end {
+        prev_state = cur_state;
+        let time = advance_state(parameters, &mut cur_state, &mut cur_time, rng);
+        if prev_state < n {
+            time_in_states[prev_state] += time;
+        }
+    }
+    if prev_state < n {
+        time_in_states[prev_state] -= cur_time - sample_end;
+    }
+    time_in_states / (sample_end - sample_start)
+}
+
+fn theory(parameters: &Parameters) -> Vector {
+    let &Parameters {
+        lambda,
+        mu,
+        n,
+        sample_start,
+        sample_end,
+    } = parameters;
+    let theta = lambda / mu;
+    let pi_0 = 1. - theta;
+    Vector::from_shape_fn(n, |k| pi_0 * theta.powi(k as i32))
+}
+
+fn main() {
+    let mut rng = Pcg64Mcg::new(SEED);
+
+    let parameters = Parameters {
+        lambda: 4.,
+        mu: 7.,
+        n: 5,
+        sample_start: 1000.,
+        sample_end: 2000.,
+    };
+
+    assert!(parameters.lambda < parameters.mu);
+    let result = test_theory(
+        experiment,
+        theory,
+        &parameters,
+        1_000_000,
+        MAX_THREADS,
+        &mut rng,
+    );
+    println!("{result:?}");
+}
---
+++ src/lib.rs
@@ -0,0 +1,98 @@
+use std::{
+    fmt::Display,
+    ops::{Add, Div},
+};
+
+use ndarray::Array1;
+use rand::{Rng, SeedableRng};
+use rand_pcg::Pcg64Mcg;
+use rayon::prelude::{IntoParallelIterator, ParallelIterator};
+
+pub type Vector = Array1<f64>;
+
+pub type ExperimentRng = Pcg64Mcg;
+
+pub trait Sample:
+    Sized + Add<Self, Output = Self> + Div<f64, Output = Self> + Send + Sync + Display
+{
+    fn mean<I>(iter: I) -> Self
+    where
+        I: IntoIterator<Item = Self>;
+}
+
+impl<T> Sample for T
+where
+    T: Sized + Add<Self, Output = Self> + Div<f64, Output = Self> + Send + Sync + Display,
+{
+    fn mean<I>(iter: I) -> Self
+    where
+        I: IntoIterator<Item = Self>,
+    {
+        let mut iter = iter.into_iter();
+
+        let mut sum = iter.next().unwrap();
+        let mut count = 1;
+        for x in iter {
+            sum = sum + x;
+            count += 1;
+        }
+        sum / count as f64
+    }
+}
+
+fn run_experiment<P, E, S>(
+    experiment: E,
+    parameters: &P,
+    num_samples: u32,
+    max_threads: u32,
+    rng: &mut impl Rng,
+) -> S
+where
+    P: Sync,
+    E: Fn(&P, &mut ExperimentRng) -> S + Sync + Send,
+    S: Sample,
+{
+    let num_samples_per_thread = num_samples / max_threads;
+    let seeds: Vec<_> = (0..max_threads).map(|_| rng.next_u64()).collect();
+
+    let total: Vec<_> = seeds
+        .into_par_iter()
+        .map(|seed| {
+            let mut rng = ExperimentRng::seed_from_u64(seed);
+            let experiment = &experiment;
+            (0..num_samples_per_thread).map(move |_| experiment(parameters, &mut rng))
+        })
+        .flatten_iter()
+        .collect();
+
+    S::mean(total)
+}
+
+#[derive(Debug)]
+pub struct TestTheoryResult<S: Sample> {
+    theoretical_result: S,
+    empirical_mean: S,
+}
+
+pub fn test_theory<P, E, S, T, R>(
+    experiment: E,
+    theory: T,
+    parameters: &P,
+    samples: u32,
+    max_threads: u32,
+    rng: &mut R,
+) -> TestTheoryResult<S>
+where
+    P: Sync,
+    E: Fn(&P, &mut ExperimentRng) -> S + Sync + Send,
+    S: Sample,
+    T: Fn(&P) -> S,
+    R: Rng,
+{
+    let empirical_mean = run_experiment(experiment, parameters, samples, max_threads, rng);
+    let theoretical_result = theory(parameters);
+    TestTheoryResult {
+        theoretical_result,
+        empirical_mean,
+    }
+}
